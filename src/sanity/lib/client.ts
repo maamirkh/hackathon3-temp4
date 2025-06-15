b0102f17ff1c3722ce31877c499cb046cebfa401
maamirkh <maamirkhan1983@gmail.com>
2025-06-16 04:05:16 +0500

    updated

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -4,8 +4,8 @@ import { createClient } from 'next-sanity'
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  dataset:  process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2025-06-15',
-  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN,
+  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN ,
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
